ad6e3b49b598e59e935f2cebca83e443b3749e89
talha khan <47978159+talhakhanbsr@users.noreply.github.com>
2019-04-29 23:15:21 -0700

    Update server.js

--- server.js
+++ server.js
@@ -1,9 +1,14 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
+//...
+//...
 
+app.use(bodyParser.urlencoded({extended:true}));
 app.set('view engine',  'ejs')
-app.get('/', function(req,res){
+app.post('/', function(req,res){
     res.render('index');
+    console.log(req.body.city);
 })
 
 app.listen(3000, function(){
